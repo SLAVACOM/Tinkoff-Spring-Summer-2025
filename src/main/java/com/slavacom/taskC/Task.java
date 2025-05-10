3167af3ccb1a2f17b811b68533de20ed29c00fb2
SLAVACOM <slavacom121@gmail.com>
2025-05-10 18:33:44 +0300

    init taskC

---
+++ src/main/java/com/slavacom/taskC/Task.java
@@ -0,0 +1,14 @@
+package main.java.com.slavacom.taskC;
+
+import java.io.*;
+
+class Task {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        writer.flush();
+        writer.close();
+    }
+}
