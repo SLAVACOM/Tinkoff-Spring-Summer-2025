b67fe85285b21b43cbdb2206fb38eada034a40ed
SLAVACOM <slavacom121@gmail.com>
2025-05-10 18:32:37 +0300

    finish taskB

--- src/main/java/com/slavacom/taskB/Task.java
+++ src/main/java/com/slavacom/taskB/Task.java
@@ -2,13 +2,46 @@ package main.java.com.slavacom.taskB;
 
 import java.io.*;
 
-class Task {
+public class Task {
+    private static class Train {
+        int firstTime;
+        int interval;
+
+        public Train(String[] input) {
+            this.firstTime = Integer.parseInt(input[0]);
+            this.interval = Integer.parseInt(input[1]);
+        }
+    }
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int n = Integer.parseInt(reader.readLine().trim());
+
+        Train[] trains = new Train[n];
+        for (int i = 0; i < n; i++) {
+            String[] input = reader.readLine().trim().split(" ");
+
+            trains[i] = new Train(input);
+        }
+
+        int q = Integer.parseInt(reader.readLine().trim());
+        for (int i = 0; i < q; i++) {
+            String[] input = reader.readLine().trim().split(" ");
+
+            int lineNum = Integer.parseInt(input[0]) - 1;
+            int time = Integer.parseInt(input[1]);
+
+            int trainWaitingTime = ((time - trains[lineNum].firstTime) % trains[lineNum].interval);
+
+            if (trainWaitingTime == 0) writer.write(time + "\n");
+            else writer.write(time + (trains[lineNum].interval - trainWaitingTime) + "\n");
+        }
 
         writer.flush();
+        reader.close();
         writer.close();
     }
 }
