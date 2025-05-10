75de74ba28024faf8db1053775255c651160bac6
SLAVACOM <slavacom121@gmail.com>
2025-05-10 17:55:10 +0300

    Init taskA

--- src/main/java/com/slavacom/taskA/Task.java
+++ src/main/java/com/slavacom/taskA/Task.java
@@ -1,10 +1,11 @@
+package main.java.com.slavacom.taskA;
+
 import java.io.*;
 
-public class Task {
+class Task {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-
 
 
         writer.flush();
