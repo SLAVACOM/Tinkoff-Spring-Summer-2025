693fdbb79dbd224930774bf4d39890a1ba7010cc
SLAVACOM <slavacom121@gmail.com>
2025-05-10 15:32:45 +0300

    Init project + base Task class + base Task.md (Description) + base TestTak class

---
+++ src/test/java/com/slavacom/tests/TestTask.java
@@ -0,0 +1,34 @@
+package test.java.com.slavacom.tests;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestTask {
+    private String runWithInput(String input) throws IOException {
+        InputStream originalIn = System.in;
+        PrintStream originalOut = System.out;
+
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setIn(in);
+        System.setOut(new PrintStream(out));
+
+//        Task.main(new String[]{});
+        System.setIn(originalIn);
+        System.setOut(originalOut);
+
+        return out.toString().trim();
+    }
+
+
+    @Test
+    public void test1() throws IOException {
+        String input = "";
+        String output = "";
+        assertEquals(output, runWithInput(input));
+    }
+
+}
